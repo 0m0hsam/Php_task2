e498432ca929c6fe139d816722603d2abee378f4
0m0hsam <60308962+0m0hsam@users.noreply.github.com>
2020-05-05 17:59:50 +0100

    New file
    New file

--- Dashboard.php
+++ Dashboard.php
@@ -1,13 +1,22 @@
 <?php
     session_start();
-    include_once ('processlog.php');
+    require_once ('processcheck.php');
     
-    
+     if(!empty($_SESSION['message'])){
+        echo"<span style= 'color:green'>". $message= $_SESSION['message']."</span>";
+    }
 ?>
 <h2>Welcome to Dashboard<h2>
+<?php
+if((@$_SESSION['position'] == "HOD") || (@$_SESSION['position'] == "REP")){
+    echo '<a href="checkfeedback.php"><button type="submit">Check all Suggesstion & Complain</button></a>';
+}else{
+    echo  '<a href="feedback.php"><button type="submit">Suggesstion & Complain</button></a>';
+}
+?>
 <p>
-<label>Username :<?php if(isset($_SESSION['firstname'])&& !empty($_SESSION['firstname']) && isset($_SESSION['lastname'])&& !empty($_SESSION['lastname'])){
-          $_SESSION['id']; echo $_SESSION['firstname']."  ".$_SESSION['lastname'];}?>
+<label>Username :<?php if(isset($_SESSION['firstname_login'])&& !empty($_SESSION['firstname_login']) && isset($_SESSION['lastname_login'])&& !empty($_SESSION['lastname_login'])){
+          $_SESSION['loged_in']; echo $_SESSION['firstname_login']."  ".$_SESSION['lastname_login'];}?>
 </label>
 </p>
 <p>
@@ -16,20 +25,29 @@
 </label>
 </p>
 <p>
-<label>login email:<?php if(isset( $_SESSION['email'])&& !empty( $_SESSION['email'])){
-           echo  $_SESSION['email'];}?>
+<label>Region:<?php if(isset( $_SESSION['region'])&& !empty( $_SESSION['region'])){
+           echo  $_SESSION['region'];}?>
+</label>
+</p>
+<p>
+<label>Province:<?php if(isset( $_SESSION['province'])&& !empty( $_SESSION['province'])){
+           echo  $_SESSION['province'];}?>
 </label>
 </p>
 
 <p>
 <label>login time:<?php echo $logout_time=date('D M Y  H:i', time()); 
 if(isset($_SESSION['logout_time']) && !empty($_SESSION['logout_time'])){
-      //use to take logout time
-    $_SESSION['logout_time']= $logout_time;}
+    //use to take logout time
+    $_SESSION['log_time']= $logout_time;}
 ?></label>
 </p>
 
 
 <p>
-<h4>Home   <a href="logout.php">Logout</a>    <a href="rest.php">Reset password</a></h4>
-</p>
+<h4><a href="dashboard.php">Home</a>   <a href="logout.php">Logout</a>    <a href="reset.php">Reset password</a> </h4>
+ 
+<?php if(@$_SESSION['position'] == "HOD"){
+    checkuser();
+             };?>
+</p
